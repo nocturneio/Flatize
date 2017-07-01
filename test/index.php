66f9dfc86f42582cc9fd11d1593037fb1e84e7f8
NHClement <clement.delouvencourt@gmail.com>
2017-07-01 16:54:40 +0200

    Add colors + landing website for doc

--- test/index.php
+++ test/index.php
@@ -1,8 +1,27 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<title>Flatize: A custom CSS Framework made for Flat Design.</title>
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+	<meta name="theme-color" content="#48cae8">
+	<meta name="description" content="A custom CSS Framework made for Flat Design."/>
+	<meta name="keywords" content="css, framework, flat, ui, gris, responsive, js, design, web"/>
+	<meta name="copyright" content="© Flatize"/>
 
-require '../vendor/autoload.php';
+	<link rel="icon" type="image/png" href="assets/favicon.png"/>
+	<link href="assets/flatize.css" rel="stylesheet"/>
 
-SassCompiler::run("../scss/", "../css/");
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+	<div class="col-12">
+		<img src="https://i.imgur.com/U9ARGaH.png">
+	</div>
+</div>
+</body>
+</html>
