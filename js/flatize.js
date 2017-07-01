d8a52fffe18c237179a6cc78f40643cad9687dbb
NHClement <clement.delouvencourt@gmail.com>
2017-07-01 20:01:05 +0200

    Add javacsript

---
+++ js/flatize.js
@@ -0,0 +1,7 @@
+/*!
+ * Flatize v0.0.1 (https://flatize.ga)
+ * Copyright - 2017 - Flatize©
+ * Licensed under MIT (https://github.com/PHClement/Flatize/blob/master/LICENSE)
+ */
+
+ import('elements/_navigation.js');
